7d6c4c13e43a06199dcee49cba30302b8c48a4e5
anthony_nobara <anthonynmui@gmail.com>
2023-02-25 18:38:44 -0500

    made rerun tests git clone

--- hw/hybrid_threads/run_tests.py
+++ hw/hybrid_threads/run_tests.py
@@ -1,5 +1,10 @@
 import unittest
+import os
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
+
+cmd = "svn export \"https://github.com/agmui/gradescope_semgrep/trunk/hybrid_threads\""
+os.system(cmd)
+os.system("cp -r hybrid_threads/* .")
 
 if __name__ == '__main__':
     suite = unittest.defaultTestLoader.discover('tests')
