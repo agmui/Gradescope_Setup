64428ee61a876f650da02d6b987ea62b6633771c
anthony_nobara <anthonynmui@gmail.com>
2023-03-03 16:42:48 -0500

    fixed leaking pattern error

--- hw/cond_vars_basic/tests/test_integration.py
+++ hw/cond_vars_basic/tests/test_integration.py
@@ -5,7 +5,10 @@ from gradescope_utils.autograder_utils.decorators import weight, tags, number
 
 
 # TODO: remove white space in file before scan
-# TODO: pattern leaking into another node
+# TODO: make scan go down then up from offset
+# TODO: when scanning there is a bug that if there is a shared pattern in two different sections and one section and the
+# top section does not contain the pattern the bottem pattern gets taken up so by consequence there is a "out of order"
+# error in the first section and a missing error in the second section
 
 # for colored output
 class bcolors:
@@ -61,8 +64,9 @@ def init_ordered(file_location: str, bounding_func: str) -> [list, int]:
 # NOTE: The formatted output is printed in sections because the function jumps from node to node.
 # So everytime it jumps is just resumes from where the last node left off. That is why there is
 # an inner_func_offset argument
+# TODO: have format_arr default to empty arr
 def graph_search(cur_node: str, func_offset: int, inner_func_offset: int, file_arr: list[str],
-                 decision_graph: dict, graph_to_pattern: dict):
+                 decision_graph: dict, graph_to_pattern: dict, format_arr):
     """
     Greedy DFS graph search for the least number of errors.
 
@@ -77,68 +81,73 @@ def graph_search(cur_node: str, func_offset: int, inner_func_offset: int, file_a
     """
     num_of_s = len(decision_graph[cur_node])  # number of successors of the current node
     if num_of_s == 0:  # terminating case
-        return {}
+        return {}, format_arr
     dict_of_err = [{}] * num_of_s  # array of the results of the errors
     num_of_err = [0] * num_of_s  # array of the number of errors each successor returns
-    print_outs = [""] * num_of_s  # array of formatted output each successor returns
+    format_arr_tries = [[""]] * num_of_s  # TODO: rename
+    file_arr_tries = [[""]] * num_of_s  # TODO: rename
     # cp of the inner_func_offset, so it does not get overwritten when running through the successors
     start_index = inner_func_offset + 1
     # print("route:", cur_node)
     for i, successor in enumerate(decision_graph[cur_node]):
-        # ===
         # print("trying:", successor)
-        # if successor == "unlock_first":
-        #     pass
-        # ===
-        dict_of_err[i], inner_func_offset, print_outs[i] = ordered_pattern(graph_to_pattern[successor],
-                                                                           func_offset + start_index,
-                                                                           file_arr[start_index:])
-        inner_func_offset += start_index
+        dict_of_err[i], inner_func_offset, temp, temp2 = ordered_pattern(
+            graph_to_pattern[successor], func_offset, start_index, file_arr, format_arr)
+        file_arr_tries[i] = temp.copy()  # FIXME:
+        format_arr_tries[i] = temp2.copy()
         if len(dict_of_err[i]) == 0:  # if 0 errors are found just print result immediately
-            print(print_outs[i], end='')
-            ret_dict = graph_search(successor, func_offset, inner_func_offset, file_arr, decision_graph,
-                                    graph_to_pattern)
-            return dict_of_err[i] | ret_dict
+            # print(print_outs[i], end='')
+            ret_dict, format_arr = graph_search(successor, func_offset, inner_func_offset, file_arr_tries[i],
+                                                decision_graph,
+                                                graph_to_pattern, format_arr_tries[i])
+            return dict_of_err[i] | ret_dict, format_arr
         num_of_err[i] = len(dict_of_err)
     successor_index = num_of_err.index(min(num_of_err))  # finding which successor had the lease number of errors
-    print(print_outs[successor_index], end='')  # printing formatted output
-    ret_dict = graph_search(decision_graph[cur_node][successor_index], func_offset, inner_func_offset, file_arr,
-                            decision_graph, graph_to_pattern)
-    return dict_of_err[successor_index] | ret_dict
+    # print(print_outs[successor_index], end='')  # printing formatted output
+    ret_dict, format_arr = graph_search(decision_graph[cur_node][successor_index], func_offset, inner_func_offset,
+                                        file_arr_tries[successor_index],
+                                        decision_graph, graph_to_pattern, format_arr_tries[successor_index])
+    return dict_of_err[successor_index] | ret_dict, format_arr
 
 
-def ordered_pattern(pattern_arr: list, total_offset: int, truncated_arr: list) -> [dict, int, str]:
+def ordered_pattern(pattern_arr: list, func_offset: int, inner_func_offset, file_arr: list, format_arr) -> [dict, int,
+                                                                                                            list, list]:
     """
     Searches for a pattern in the truncated array.
     Total offset is so the formatted output has the correct line numbers relative to the original file.
 
     :param pattern_arr: the pattern list that is to be searched
-    :param total_offset: total offset from the top of the file
-    :param truncated_arr: the portion of the file that will be searched
+    :param func_offset: total offset from the top of the file
+    :param file_arr: the portion of the file that will be searched
     :return: dict of errors, last pattern line number it stopped at, formatted output for terminal
     """
     # ========================= substring search ========================
-    code_arr = truncated_arr.copy()
-    format_arr: list = ['n'] * len(code_arr)  # for printing output
-    past = -1
-    line_num = 0
-    errors = {}
+    code_arr = file_arr.copy()
+    # format_arr: list = ['n'] * len(code_arr)  # for printing output
+    past = inner_func_offset - 1
+    line_num = inner_func_offset
+    errors_dict = {}
     for sub_str in pattern_arr:
+        missing: bool = True
         if not isinstance(sub_str, list):  # to wrap substring in arr
             sub_str = [sub_str]
         for comment_index in sub_str:
+            # if sub_str[0] == "pthread_cond_signal(.*)":
+            #     pass
             for c in ['(', ')', '+', '=']:  # formatting input for re
                 comment_index = comment_index.replace(c, '\\' + c)
             arr = [line for line in code_arr if re.findall(comment_index, line)]
             if len(arr) != 0:
-                line_num = code_arr.index(arr[0])
+                missing = False
+                line_num = code_arr.index(arr[0])  # NOTE: only uses first fine
                 code_arr[line_num] = ""  # delete line so it does not get used again
                 break
+        # ========================= error detection ========================
         error_str = ""
         if line_num < past:
             format_arr[line_num] = 'w'
             error_str = "out of order line: " + str(line_num)
-        elif line_num == past or (past == -1 and line_num == 0):
+        elif missing:  # line_num == past or (past == inner_func_offset - 1 and line_num == inner_func_offset):
             if type(format_arr[line_num]) == str:
                 format_arr[line_num] = sub_str
             else:
@@ -149,14 +158,13 @@ def ordered_pattern(pattern_arr: list, total_offset: int, truncated_arr: list) -
 
         # error formating
         if error_str:
-            if sub_str[0] not in errors:
-                errors[sub_str[0]] = [error_str]
+            if sub_str[0] not in errors_dict:
+                errors_dict[sub_str[0]] = [error_str]
             else:
-                errors[sub_str[0]].append(error_str)
+                errors_dict[sub_str[0]].append(error_str)
 
         past = line_num
 
-    # ========================= formatting output ========================
     # finding last searched pattern
     last_pattern = 0
     for i, c in enumerate(reversed(format_arr)):
@@ -164,8 +172,15 @@ def ordered_pattern(pattern_arr: list, total_offset: int, truncated_arr: list) -
             last_pattern = len(format_arr) - i - 1
             break
 
+    return errors_dict, last_pattern, code_arr, format_arr
+
+
+def format_output(file_arr, format_arr, total_offset):
     print_out = ""
-    for i, line in enumerate(truncated_arr):
+    # total_offset = func_offset + inner_func_offset
+    # if inner_func_offset == 1:  # to print out first line of the func
+    #     inner_func_offset = 0
+    for i, line in enumerate(file_arr):
         match format_arr[i]:
             case 'o':
                 print_out += f'{bcolors.OKGREEN}{total_offset + i + 1:4d} | {line}{bcolors.ENDC}\n'
@@ -176,10 +191,9 @@ def ordered_pattern(pattern_arr: list, total_offset: int, truncated_arr: list) -
             case _:  # missing/error case
                 print_out += f'{total_offset + i + 1:4d} | {line}\n'
                 print_out += f'{bcolors.FAIL} missing {format_arr[i]}{bcolors.ENDC}\n'
-        if i == last_pattern:
-            return errors, last_pattern, print_out
-
-    return errors, last_pattern, print_out
+        # if i == last_pattern:
+        #     return print_out
+    return print_out
 
 
 # ========== inorder ==========
@@ -252,7 +266,10 @@ max_graph_convert = {
 }
 
 truncated_file_arr, offset = init_ordered("../src/max.c", "void *thread(void *arg)")
-errors = graph_search('head', offset, 0, truncated_file_arr, max_decision_graph, max_graph_convert)
+format_arr: list = ['n'] * len(truncated_file_arr)  # for printing output
+errors, format_arr = graph_search('head', offset, 0, truncated_file_arr, max_decision_graph, max_graph_convert,
+                                  format_arr)
+print(format_output(truncated_file_arr, format_arr, offset))
 print("errors:", errors)
 
 # ========== prodcons ==========
