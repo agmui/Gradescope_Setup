97572dbec230f88a3f43bbed8447b31b825013d6
anthony_nobara <anthonynmui@gmail.com>
2023-03-08 00:39:33 -0500

    added ls for debuging

--- hw/c_review/tests/test_integration.py
+++ hw/c_review/tests/test_integration.py
@@ -189,6 +189,8 @@ def format_output(file_arr, format_arr, total_offset):
                 print(f'{bcolors.FAIL} missing {format_arr[i]}{bcolors.ENDC}\n', end='')
 
 
+os.system("ls")
+os.system("ls src")
 os.chdir("src/user")
 class TestIntegration(unittest.TestCase):
     def setUp(self):
