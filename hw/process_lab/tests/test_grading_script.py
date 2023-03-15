c91745238549e037cb3a475d461f298b4bd5a191
anthony_nobara <anthonynmui@gmail.com>
2023-03-14 21:46:38 -0400

    fixed tests in process lab

--- hw/process_lab/tests/test_grading_script.py
+++ hw/process_lab/tests/test_grading_script.py
@@ -5,8 +5,9 @@ from gradescope_utils.autograder_utils.decorators import weight, tags, number, p
 from art import *
 import re
 
-
 os.chdir("src")
+
+
 class TestIntegration(unittest.TestCase):
     def setUp(self):
         print("If you have questions go to: https://www.youtube.com/watch?v=xvFZjo5PgG0 for help")
@@ -17,12 +18,16 @@ class TestIntegration(unittest.TestCase):
         """autograder simple_tests"""
         print(text2art("process lab", "rand"))
         os.system("make simple_test")
-        print("if the autograder does not work email muian@rose-hulman.edu and tell them they are an idiot and tell them to fix it\n")
+        print(
+            "if the autograder does not work email muian@rose-hulman.edu and tell them they are an idiot and tell them to fix it\n")
         process = subprocess.Popen(['./simple_test'], stdout=subprocess.PIPE, encoding='UTF-8')
         result, error = process.communicate()
         print(result)
-        self.assertTrue("Test Passed" in result, "one of the tests did not pass")
-        self.assertTrue("Test Timed Out" in result, "tests did not time out")
+
+        result = result.lower()
+        self.assertTrue(re.search("test( \d)? passed", result), "did not contain \"Test Passed\" in output")
+        self.assertTrue(re.search("test crashed", result), "a test did not crash")
+        self.assertTrue(re.search("test timed? out", result), "a test did not time out")
 
 
 if __name__ == '__main__':
