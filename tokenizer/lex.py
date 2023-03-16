28b8eaec981c9aa89e20cb6b3c825dc9e5aa9e1c
anthony_nobara <anthonynmui@gmail.com>
2023-03-16 11:34:12 -0400

    updated format

--- tokenizer/lex.py
+++ tokenizer/lex.py
@@ -1,12 +1,12 @@
 from typing import NamedTuple, Type
-import re
+import re, os
 import minify
 
 
 class Exp:
     def __init__(self, type, value, arg, line, column):
         self.type: str = type
-        self.value: str = value
+        self.value: any = value
         self.arg: list = arg
         self.line: int = line
         self.column: int = column
@@ -32,7 +32,7 @@ def back_prop(var_name, line_num):
     truncated_min = "\n".join(min_file_arr[:line_num])
     truncated_code = re.split('[;{}]', truncated_min)
     for l in reversed(truncated_code):  # searching through full file
-        ex = eval(l, line_num, just_kind=True)  # TODO: fix line_num
+        ex = eval(l, line_num)  # TODO: fix line_num
         print(ex)
         if ex is None:
             continue
@@ -46,85 +46,85 @@ def back_prop(var_name, line_num):
     return ex
 
 
-def eval(expression: str, line_num, just_kind: bool = False):
+def eval(expression: str, line_num: int, just_kind: bool = False) -> Exp:
     name = r'[a-zA-Z_-][\w-]*'  # for names of func/var and data types
     token_specification = [
         ('NEWLINE', r'\n'),  # Line endings
         ('SKIP', r'[ \t{}]+'),  # Skip over spaces and tabs
         ('ASSIGN', fr'({name})=(.*)'),  # Assignment operator
         ('NUMBER', r'^(\d+(\.\d*)?)'),  # Integer or decimal number
+        # ('BOOL', r'^(true|false)'),  # bool
         # ('END',      r';'),            # Statement terminator
         ('FUNC', r'(return) ?(.*)'),  # return
-        ('CLOSURE', fr'^{name} ({name})\((.*)\)'),  # function declaration
+        ('CLOSURE', fr'{name} ({name})\((.*)\)'),  # function declaration TODO: add if, for, and while loop
+        ('CLOSURE', fr'(if|for|while)\((.*)\)'),  # function declaration TODO: add if, for, and while loop
         ('DEC', fr'(^{name} {name})'),  # declare variable
         ('FUNC', fr'({name})\((.*)\)'),  # function call
         ('VAR', fr'({name})'),  # Variable (that needs to be derived)
         # ('OP', r'[+\-*/]'),  # Arithmetic operators TODO: add comparators
-        # ('MISMATCH', r'.'),  # Any other character
+        ('MISMATCH', r'.'),  # Any other character
     ]
+    line_start = 0
     print("input:", [expression])
-    column = 0
     if expression == '':
-        return
-    for reg_try in token_specification:
-        kind = reg_try[0]
-        regex = reg_try[1]
+        return None
+    for tokens in token_specification:
+        kind = tokens[0]
+        regex = tokens[1]
+        arg: any = None
         if m := re.search(regex, expression):
+            column = m.start() - line_start
+            value: any = None
             if kind != "SKIP" and kind != "NEWLINE":
                 print("exp:", [expression], "| groups:", m.groups(), 'kind:', kind, )
+                value: any = m[1]  # TODO move
             match kind:
                 case "FUNC":
-                    t = Exp(kind, m[1], m.group(2), line_num, column)
-                    if just_kind:
-                        return t
-                    if t.arg:
-                        t.arg = eval(t.arg, line_num)
-                    print(t)
-                    return t
-                case 'CLOSURE':
-                    t = Exp(kind, m[1], m.group(2), line_num, column)
-                    if just_kind:
-                        return t
                     arg = m.group(2)
-                    if arg:
-                        arg = re.findall(fr'({name} {name})', m.group(2))
-                        t.arg = eval(arg, line_num)
-                    print(t)
-                    return t
-                case 'DEC':
-                    t = Exp(kind, m[1], [], line_num, column)
-                    if just_kind:
-                        return t
-                    print(t)
-                    return t
+                    if not just_kind and arg:
+                        arg = eval(arg, line_num)
+                case 'CLOSURE':
+                    arg = m.group(2)
+                    if not just_kind and arg:
+                        # TODO: handle arg conditions for loops and ifs
+                        args = re.findall(fr'({name} {name})', m.group(2))
+                        arr = []
+                        for i in args:  # evaluate each argument individually
+                            arr.append(eval(i, line_num))
+                        arg = arr
+                # case 'DEC':
+                #     value = m[1]
+                #     if just_kind:
+                #         return t
+                #     print(t)
+                #     return t
                 case 'VAR':
-                    # t = Exp(kind, m[1], [], line_num, column)
+                    if value in keywords:
+                        break
                     t = back_prop(m[1], line_num)
-                    if just_kind:
-                        return t
-                    print(t)
                     return t
                 case 'ASSIGN':
-                    t = Exp(kind, m[1], m[2], line_num, column)
-                    # t.arg = back_prop(line_num-1, t.arg)
-                    t.arg = eval(t.arg, line_num)
-                    if just_kind:
-                        return t
-                    print(t)
-                    return t
+                    arg = m[2]
+                    if not just_kind:
+                        arg = eval(arg, line_num)
                 case 'NUMBER':
-                    t = Exp(kind, m[1], [], line_num, column)
-                    if just_kind:
-                        return t
-                    print(t)
-                    return t
+                    value = float(value) if '.' in value else int(value)
+                case 'BOOL':
+                    value = "true" == value
                 case 'NEWLINE':
                     line_num -= 1
+                    line_start = m.end()
                     continue
                 case 'SKIP':
                     continue
+                case 'MISMATCH':
+                    raise RuntimeError(f'{value!r} unexpected on line {line_num}')
+            t = Exp(kind, value, arg, line_num, column)
+            print(t)
+            return t
 
 
+# os.system("gcc test.c")
 f = open('test.c')
 file = f.read()
 f.close()
@@ -133,7 +133,7 @@ min_file_arr = minify.minify_source(file_arr)
 min_file = "\n".join(min_file_arr)
 code = re.split('[;{}]', min_file)
 # code = re.split('[;{}]', file)
-print("code",code)
+print("code", code)
 ans = []
 for i, line in enumerate(reversed(code)):
     print("------")
