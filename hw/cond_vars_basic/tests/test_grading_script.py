a0d26cd361ef0a09854307c68ce146d6f60dce98
anthony_nobara <anthonynmui@gmail.com>
2023-04-18 11:39:12 -0400

    finish writing tests for tunnel

--- hw/cond_vars_basic/tests/test_grading_script.py
+++ hw/cond_vars_basic/tests/test_grading_script.py
@@ -4,6 +4,7 @@ import unittest
 from gradescope_utils.autograder_utils.decorators import weight, tags, number, partial_credit
 from art import *
 import asyncio  # TODO: add to reqirements.txt
+import re
 
 
 async def async_run_cmd(cmd):
@@ -31,7 +32,9 @@ async def run():
     return await asyncio.gather(
         async_run_cmd('./inorder'),
         async_run_cmd('./max'),
-        async_run_cmd('./prodcons_condvar')
+        async_run_cmd('./prodcons_condvar'),
+        async_run_cmd('./rooms'),
+        async_run_cmd('./tunnel')
     )
     # output = [0, 0, 0]
     # task1 = asyncio.create_task(async_run_cmd('./inorder'))
@@ -49,7 +52,7 @@ os.system("make clean")
 os.system("make")
 
 print("===starting tests===")
-output1, output2, output3 = asyncio.run(run())
+output1, output2, output3, output4, output5 = asyncio.run(run())
 
 
 # print("====================")
@@ -142,6 +145,100 @@ class TestIntegration(unittest.TestCase):
         else:
             print("\noutput correct!")
             self.assertTrue(True)
+
+    def test_rooms_output(self):
+        print("======output checking=======")
+        output = output4["output"].split('\n')
+        error = False
+        room1 = 0
+        waiting_room2 = 0
+        room2 = 0
+        unhappy = 0
+        final_number = 0
+        for i in output:
+            print(i)
+            if "Just arrived" in i:
+                continue
+            elif "Entered first room" in i:
+                room1 += 1
+                if room1 > 5:
+                    print("=== ERROR: number of people in room1:", room1, "===")
+                    error = True
+            elif "Left first room" in i:
+                room1 -= 1
+            elif "Joined the waiting room for second room" in i:
+                waiting_room2 += 1
+                if waiting_room2 > 2:
+                    print("=== ERROR: number of people in waiting room:", waiting_room2, "===")
+                    error = True
+            elif "Entered second room" in i:
+                if waiting_room2 > 0:
+                    waiting_room2 -= 1
+                room2 += 1
+                if room2 > 5:
+                    print("=== ERROR: number of people in room2:", room2, "===")
+                    error = True
+            elif "Left second room" in i:
+                room2 -= 1
+            elif "waiting room is full" in i:
+                unhappy += 1
+                if waiting_room2 < 2:
+                    print("=== ERROR: waiting room was not full:", waiting_room2, "===")
+                    error = True
+            elif "Everything finished" in i:
+                final_number = int(re.findall(r'\d+', i)[0])
+                continue
+            else:
+                print("unknown output:", i)
+
+        if final_number > 6 or 3 > final_number:
+            print("number of unhappy customers is sus:", final_number)
+
+        self.assertTrue(not error)
+
+    def test_tunnel_output(self):
+        output = output5["output"].split('\n')
+        EW_num = 0
+        WE_num = 0
+        Ambulances_inside = 0
+        for i in output:
+            print(i)
+            # for cars
+            if "Car" in i:
+                if "entered tunnel in EW" in i:
+                    if Ambulances_inside > 0:
+                        print("=== ERROR", "===")
+                    EW_num += 1
+                elif "exited tunnel in EW" in i:
+                    EW_num -= 1
+                elif "entered tunnel in WE" in i:
+                    if Ambulances_inside > 0:
+                        print("=== ERROR", "===")
+                    WE_num += 1
+                elif "exited tunnel in WE" in i:
+                    WE_num -= 1
+                else:
+                    print("unknown output", i)
+            # for ambulance
+            elif "Ambulance" in i:
+                if "entered tunnel in EW" in i:
+                    Ambulances_inside += 1
+                    EW_num += 1
+                elif "exited tunnel in EW" in i:
+                    Ambulances_inside -= 1
+                    EW_num -= 1
+                elif "entered tunnel in WE" in i:
+                    Ambulances_inside += 1
+                    WE_num += 1
+                elif "exited tunnel in WE" in i:
+                    Ambulances_inside -= 1
+                    WE_num -= 1
+                else:
+                    print("unknown output", i)
+            if EW_num > 3:
+                print("=== ERROR", "===")
+            if WE_num > 1:
+                print("=== ERROR", "===")
 
 
 if __name__ == '__main__':
