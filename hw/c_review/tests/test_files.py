bf0701aa02bebcc9088dfe292e404a8368215555
agmui <anthonynmui@gmail.com>
2023-12-06 20:48:15 -0500

    changed file checking to glob

--- hw/c_review/tests/test_files.py
+++ hw/c_review/tests/test_files.py
@@ -1,16 +1,24 @@
+import os.path
 import unittest
 from gradescope_utils.autograder_utils.decorators import weight
 from gradescope_utils.autograder_utils.files import check_submitted_files
+import glob
 
 
 class TestFiles(unittest.TestCase):
     @weight(0)
     def test_submitted_files(self):
         """Check submitted files"""
-        missing_files = check_submitted_files([
-            'submission-*.zip'
-            #'submit-lab-0.tar','submit-lab-0.patch'
-            ])
+        # missing_files = check_submitted_files([
+        #     'submission-*.zip'
+        #     #'submit-lab-0.tar','submit-lab-0.patch'
+        #     ])
+        SUBMISSION_BASE = '/autograder/submission'
+        files_to_check = ['submission-*.zip']
+        missing_files = []
+        for file in files_to_check:
+            if len(glob.glob(os.path.join(SUBMISSION_BASE, file))) == 0:
+                missing_files.append(file)
         for path in missing_files:
             print('Missing {0}'.format(path))
         self.assertEqual(len(missing_files), 0, 'Missing some required files!')
