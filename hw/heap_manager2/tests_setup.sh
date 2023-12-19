89d9ebb42fd7bccf3d2a7ca0adb9d36232f13ef5
agmui <anthonynmui@gmail.com>
2023-12-19 10:06:45 -0500

    fixed svn branch

--- hw/heap_manager2/tests_setup.sh
+++ hw/heap_manager2/tests_setup.sh
@@ -3,7 +3,7 @@
 # shellcheck disable=SC2164
 SRC_DIR=/autograder/source/src
 cd $SRC_DIR
-svn export https://github.com/rhit-csse332/csse332-labs/branches/clab/xv6-riscv/ > /dev/null
+svn export https://github.com/rhit-csse332/csse332-labs/branches/buddy/xv6-riscv/ > /dev/null
 if test -f $SRC_DIR/user/*.c; then # TODO: add this to all hws
   echo ur one of the gud ones c:
   mv $SRC_DIR/user/*.c $SRC_DIR/xv6-riscv/user/
