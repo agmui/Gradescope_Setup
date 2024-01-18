b2ac0a8b63029ef2e0643df926e24dd632b43def
agmui <anthonynmui@gmail.com>
2024-01-18 16:38:30 -0500

    added clone_dir command and fixed run_autograder

--- autograder/setup.sh
+++ autograder/setup.sh
@@ -1,34 +1,29 @@
 #!/usr/bin/env bash
 
-#TODO: check if you can read json
-grep -Po '"title":.*?",' $AUTOGRADER_ROOT/submission_metadata.json | cut -d':' -f 2 | tr -d '"' | tr -d ',' | awk '{$1=$1;print}' >> assignmentName.txt
-assignment="heap_manager2"
-ls autograder/source
+#ls autograder/source
 
 apt update -y > /dev/null && apt upgrade -y
 apt install -y build-essential gdb-multiarch qemu-system-misc gcc-riscv64-linux-gnu binutils-riscv64-linux-gnu # for xv6 labs
 apt install -y cowsay expect # for simpleshell labs
 
-function clone_dir() {
-  BRANCH=$1
-  DIR=$2
-  URL=$3
-  # --no-checkout avoids us from downloading unneeded junk
-  git clone --no-checkout $URL
-  cd gradescope_semgrep
-  git checkout $BRANCH
-  #setups sparse checkout https://github.blog/2020-01-17-bring-your-monorepo-down-to-size-with-sparse-checkout/
-  git sparse-checkout set --cone
-  git sparse-checkout set $DIR #clones only xv6-riscv/ DIR
-}
-clone_dir main hw/$assignment https://github.com/agmui/gradescope_semgrep
-cd ..
-ls
-pwd
-ls ..
-mv ./gradescope_semgrep/hw/$assignment/* /autograder/source
+#function clone_dir() {
+#  BRANCH=$1
+#  DIR=$2
+#  URL=$3
+#  # --no-checkout avoids us from downloading unneeded junk
+#  git clone --no-checkout $URL
+#  cd gradescope_semgrep
+#  git checkout $BRANCH
+#  #setups sparse checkout https://github.blog/2020-01-17-bring-your-monorepo-down-to-size-with-sparse-checkout/
+#  git sparse-checkout set --cone
+#  git sparse-checkout set $DIR #clones only xv6-riscv/ DIR
+#}
+#clone_dir main hw/$assignment https://github.com/agmui/gradescope_semgrep
+
 #svn checkout https://github.com/agmui/gradescope_semgrep/trunk/hw/$assignment /autograder/source
 
-apt-get install -y python3 python3-pip python3-dev
+git clone https://github.com/agmui/gradescope_semgrep
 
-pip3 install -r /autograder/source/requirements.txt
+apt-get install -y python3 python3-pip python3-dev jq
+
+mv /autograder/source/clone_dir /usr/bin
--- hw/heap_manager2/tests_setup.sh
+++ hw/heap_manager2/tests_setup.sh
@@ -4,20 +4,6 @@
 SRC_DIR=/autograder/source/src
 cd $SRC_DIR
 
-# first arg is branch second is directory
-function clone_dir() {
-  BRANCH=$1
-  DIR=$2
-  URL=$3
-  # --no-checkout avoids us from downloading unneeded junk
-  git clone --no-checkout $URL
-  cd csse332-labs
-  git checkout $BRANCH
-  #setups sparse checkout https://github.blog/2020-01-17-bring-your-monorepo-down-to-size-with-sparse-checkout/
-  git sparse-checkout set --cone
-  git sparse-checkout set $DIR #clones only xv6-riscv/ DIR
-  cd ..
-}
 clone_dir buddy xv6-riscv https://github.com/rhit-csse332/csse332-labs.git
 #SVN is no longer supported (https://github.blog/2023-01-20-sunsetting-subversion-support/)
 #svn export https://github.com/rhit-csse332/csse332-labs/branches/buddy/xv6-riscv/ > /dev/null
@@ -36,4 +22,4 @@ fi
 cd $SRC_DIR/csse332-labs/xv6-riscv/
 ls
 pwd
-make > /dev/null
+make qemu> /dev/null
