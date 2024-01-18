42881f73c97bdf165347ef834221548c3fdbf35f
agmui <anthonynmui@gmail.com>
2024-01-18 18:02:36 -0500

    help

--- hw/heap_manager2/tests/test_grading_script.py
+++ hw/heap_manager2/tests/test_grading_script.py
@@ -6,7 +6,6 @@ from art import *
 from gradelib import *
 from gradelib import TESTS
 
-print('test_grading_script.py')
 os.chdir("src/csse332-labs/xv6-riscv")
 # os.system("apt update -y > /dev/null && apt upgrade -y > /dev/null")
 # os.system(
