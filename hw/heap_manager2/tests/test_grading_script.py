d1486ab4a7256af1b67fa95b95a9b695561e1659
agmui <anthonynmui@gmail.com>
2024-01-19 09:38:54 -0500

    removed 2 unittest.main() calls

--- hw/heap_manager2/tests/test_grading_script.py
+++ hw/heap_manager2/tests/test_grading_script.py
@@ -64,12 +64,6 @@ def test_stress_overlapping():
     r.match("^test_stress_overlapping\\(\\d+\\): OK\.",
             no=[".*Assertion FAILED.*"])
 
-try:
-    run_tests()
-finally:
-    print('failed tests')
-    pass
-    # unittest.main()
 
 
 class TestIntegration(unittest.TestCase):
@@ -116,4 +110,8 @@ class TestIntegration(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main()
+    try:
+        run_tests()
+    finally:
+        unittest.main()
+    # unittest.main()
