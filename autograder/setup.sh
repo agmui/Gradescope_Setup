6866f12a6c6024bf19237ac25d5c3c4ade891c2a
agmui <anthonynmui@gmail.com>
2024-01-19 09:47:25 -0500

    removed extra msgs

--- autograder/setup.sh
+++ autograder/setup.sh
@@ -6,22 +6,6 @@ apt update -y > /dev/null && apt upgrade -y
 apt install -y build-essential gdb-multiarch qemu-system-misc gcc-riscv64-linux-gnu binutils-riscv64-linux-gnu # for xv6 labs
 apt install -y cowsay expect # for simpleshell labs
 
-#function clone_dir() {
-#  BRANCH=$1
-#  DIR=$2
-#  URL=$3
-#  # --no-checkout avoids us from downloading unneeded junk
-#  git clone --no-checkout $URL
-#  cd gradescope_semgrep
-#  git checkout $BRANCH
-#  #setups sparse checkout https://github.blog/2020-01-17-bring-your-monorepo-down-to-size-with-sparse-checkout/
-#  git sparse-checkout set --cone
-#  git sparse-checkout set $DIR #clones only xv6-riscv/ DIR
-#}
-#clone_dir main hw/$assignment https://github.com/agmui/gradescope_semgrep
-
-#svn checkout https://github.com/agmui/gradescope_semgrep/trunk/hw/$assignment /autograder/source
-
 git clone https://github.com/agmui/gradescope_semgrep
 
 apt-get install -y python3 python3-pip python3-dev jq
--- hw/heap_manager2/tests_setup.sh
+++ hw/heap_manager2/tests_setup.sh
@@ -8,8 +8,6 @@ clone_dir buddy xv6-riscv https://github.com/rhit-csse332/csse332-labs.git
 #SVN is no longer supported (https://github.blog/2023-01-20-sunsetting-subversion-support/)
 #svn export https://github.com/rhit-csse332/csse332-labs/branches/buddy/xv6-riscv/ > /dev/null
 
-pwd
-ls
 #if test -f $SRC_DIR/user/*.c; then # TODO: add this to all hws
 #  echo ur one of the gud ones c:
 #  mv $SRC_DIR/user/*.c $SRC_DIR/csse332-labs/xv6-riscv/user/
@@ -20,7 +18,7 @@ ls
 #  echo Y U ZIP WRONG RAWWWWWWW >:c
 #  mv $SRC_DIR/*.c $SRC_DIR/csse332-labs/xv6-riscv/user/
 #fi
-cp -r $SRC_DIR/* $SRC_DIR/csse332-labs/xv6-riscv/
+cp -r $SRC_DIR/* $SRC_DIR/csse332-labs/xv6-riscv/ > /dev/null
 
 cd $SRC_DIR/csse332-labs/xv6-riscv/
 make > /dev/null
