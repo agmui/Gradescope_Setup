53cb2ec7a57498d2f7614dd6261339a4e15cedca
agmui <anthonynmui@gmail.com>
2024-01-19 09:44:06 -0500

    moved text2art

--- hw/vmem2/tests/test_files.py
+++ hw/vmem2/tests/test_files.py
@@ -1,4 +1,6 @@
 import unittest
+
+from art import text2art
 from gradescope_utils.autograder_utils.decorators import weight
 from gradescope_utils.autograder_utils.files import check_submitted_files
 
@@ -7,6 +9,7 @@ class TestFiles(unittest.TestCase):
     @weight(0)
     def test_submitted_files(self):
         """Check submitted files"""
+        print(text2art("heap manager", "rand"))
         missing_files = check_submitted_files(['submit-lab-5.tar','submit-lab-5.patch'])
         for path in missing_files:
             print('Missing {0}'.format(path))
