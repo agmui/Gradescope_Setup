25f8824db661dfbc2f51cbc6ca09b3e5464276b9
agmui <anthonynmui@gmail.com>
2024-01-19 16:08:32 -0500

    re added flag

--- hw/lab07-threads_mutex_basic/tests_setup.sh
+++ hw/lab07-threads_mutex_basic/tests_setup.sh
@@ -6,7 +6,7 @@ SRC_DIR=/autograder/source/src
 cd $SRC_DIR
 #make > /dev/null
 
-gcc -Wall -g -pthread -c -pthread factoring.c -o thread_factoring.bin
-gcc -Wall -g -pthread -c -pthread threadSort.c -o thread_sort.bin
-gcc -Wall -g -pthread -c -pthread add_a_lot.c -o basic_mutex.bin
-gcc -Wall -g -pthread -c -pthread red_blue_purple.c -o red_blue_purple.bin
+gcc -Wall -g -pthread -c -pthread -ggdb factoring.c -o thread_factoring.bin
+gcc -Wall -g -pthread -c -pthread -ggdb threadSort.c -o thread_sort.bin
+gcc -Wall -g -pthread -c -pthread -ggdb add_a_lot.c -o basic_mutex.bin
+gcc -Wall -g -pthread -c -pthread -ggdb red_blue_purple.c -o red_blue_purple.bin
