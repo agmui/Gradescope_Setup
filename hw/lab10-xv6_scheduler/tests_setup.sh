60fab958c3fa30d2c84d3c1b958cdb14b5e79106
agmui <anthonynmui@gmail.com>
2024-01-29 16:03:32 -0500

    added scheduler activity to run_autograder

--- hw/lab10-xv6_scheduler/tests_setup.sh
+++ hw/lab10-xv6_scheduler/tests_setup.sh
@@ -4,8 +4,10 @@
 SRC_DIR=/autograder/source/src
 cd $SRC_DIR
 
-clone_dir clab xv6-riscv https://github.com/rhit-csse332/csse332-labs.git
+clone_dir klist xv6-riscv https://github.com/rhit-csse332/csse332-labs.git
 
-mv $SRC_DIR/kernel/*.c $SRC_DIR/csse332-labs/xv6-riscv/kernel/
+cp -r $SRC_DIR/* $SRC_DIR/csse332-labs/xv6-riscv/ > /dev/null
 cd $SRC_DIR/csse332-labs/xv6-riscv/
+make clean > /dev/null
 make > /dev/null
+echo ran make
