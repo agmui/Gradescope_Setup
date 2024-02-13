b8239fb773e56f02da2fa22738bde17bf91bae1a
agmui <anthonynmui@gmail.com>
2024-02-13 13:48:42 -0500

    fixed subprocess

--- hw/lab12-ext2/tests/test_grading_script.py
+++ hw/lab12-ext2/tests/test_grading_script.py
@@ -9,7 +9,6 @@ from gradelib import TESTS
 import re
 
 os.chdir("src/csse332-labs/ext2")
-os.system('ls;pwd')
 
 class TestIntegration(unittest.TestCase):
     def setUp(self):
@@ -20,7 +19,7 @@ class TestIntegration(unittest.TestCase):
     def test_filereader(self):
         """autograder file reader tests"""
         print(text2art("ext2 lab!!!", "rand"))
-        process = subprocess.Popen(['ls;pwd;./filereader 451_filesystem.ext2 /small-file.txt'], stdout=subprocess.PIPE,
+        process = subprocess.Popen(['./filereader','451_filesystem.ext2','/small-file.txt'], stdout=subprocess.PIPE,
                                    encoding='UTF-8')
         result, error = process.communicate()
         print(result)
