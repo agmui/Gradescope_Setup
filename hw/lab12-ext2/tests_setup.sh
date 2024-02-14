2886d3750e45027a80361a62e249973b3338aeb6
agmui <anthonynmui@gmail.com>
2024-02-13 19:28:54 -0500

    fixed removing ext2

--- hw/lab12-ext2/tests_setup.sh
+++ hw/lab12-ext2/tests_setup.sh
@@ -5,7 +5,7 @@ SRC_DIR=/autograder/source/src
 cd $SRC_DIR
 
 clone_dir main ext2 https://github.com/rhit-csse332/csse332-labs.git
-rm $SRC_DIR/ext2access.c
+rm $SRC_DIR/csse332-labs/ext2/ext2access.c
 
 cp -r $SRC_DIR/* $SRC_DIR/csse332-labs/ext2/ > /dev/null
 cd $SRC_DIR/csse332-labs/ext2/
