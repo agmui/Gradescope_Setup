c0281a973bea03ca7eb0ef5250afd07058c634ed
agmui <anthonynmui@gmail.com>
2024-02-18 14:17:08 -0500

    added print

--- hw/lab10-xv6_scheduler/run_tests.py
+++ hw/lab10-xv6_scheduler/run_tests.py
@@ -3,6 +3,7 @@ import os
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 if __name__ == '__main__':
+    print("help")
     os.chdir("src/csse332-labs/xv6-riscv")
     suite = unittest.defaultTestLoader.discover('tests')
     with open('/autograder/results/results.json', 'w') as f:
