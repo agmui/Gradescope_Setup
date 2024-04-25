f73ac31a9e8eb1d5e69026bd590af7d6cfb1514d
agmui <anthonynmui@gmail.com>
2024-04-25 13:15:39 -0400

    added printout for scheduler lab

--- hw/lab10-xv6_scheduler/tests/test_grading_script.py
+++ hw/lab10-xv6_scheduler/tests/test_grading_script.py
@@ -1,12 +1,18 @@
 import os
 import subprocess
+import sys
 import unittest
 from gradescope_utils.autograder_utils.decorators import weight, tags, number, partial_credit
 from art import *
 import asyncio # TODO: add to reqirements.txt
-from gradelib import *
-from gradelib import TESTS
+# from gradelib import *
+# from gradelib import TESTS
 import re
+
+sys.path.insert(0, '../..')  # adds the hw project dir to the python path
+from hw.grading_utils.gradelib import *  # this is allowed bc of the sys.path.insert
+from hw.grading_utils.gradelib import TESTS
+from hw.grading_utils.random_utils import capture_output
 
 os.chdir("src/csse332-labs/xv6-riscv")
 
@@ -23,7 +29,9 @@ def test_simple():
     matches = re.findall("^test forkforkfork: OK$", r.qemu.output, re.M)
     assert_equal(len(matches), 1, "Number of appearances of 'forkforkfork: OK'")
 
-run_tests()
+output, error = capture_output(run_tests)
+output_arr = output.split('\n')
+
 class TestIntegration(unittest.TestCase):
     def setUp(self):
         pass
@@ -31,13 +39,8 @@ class TestIntegration(unittest.TestCase):
     @weight(0)
     @number("1")
     def test_make_grade(self):
-        """autograder us1tests.c tests"""
-        # os.system("sudo apt install -y build-essential gdb-multiarch qemu-system-misc gcc-riscv64-linux-gnu binutils-riscv64-linux-gnu")
-        # os.system("tar xf submit-lab-sched.tar")
-        # process = subprocess.Popen(['make grade'], stdout=subprocess.PIPE, encoding='UTF-8')
-        # result, error = process.communicate()
-        # print(result)
-        # self.assertTrue("OK" in result)
+        """autograder forkforkfork tests"""
+        print(output_arr[0])
         self.assertTrue(TESTS[0].ok)
 
 
