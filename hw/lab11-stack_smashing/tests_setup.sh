bc73a7f07ca23ba5085565db2a301ef44965542e
agmui <anthonynmui@gmail.com>
2024-05-06 23:41:20 -0400

    fixed stack smashing

--- hw/lab11-stack_smashing/tests_setup.sh
+++ hw/lab11-stack_smashing/tests_setup.sh
@@ -7,10 +7,10 @@ clone_dir clab xv6-riscv https://github.com/rhit-csse332/csse332-labs.git
 
 
 #mv $SRC_DIR/user/*.c $SRC_DIR/csse332-labs/xv6-riscv/user/
-cp -r /autograder/submission/* $SRC_DIR/csse332-labs/xv6-riscv/
+cp -r /autograder/submission/* $SRC_DIR
 
-cd $SRC_DIR/csse332-labs/xv6-riscv/
-make > /dev/null
+cd $SRC_DIR
+#make > /dev/null
 
 cd $SRC_DIR/..
 echo "--- running run_tests.py ---"
