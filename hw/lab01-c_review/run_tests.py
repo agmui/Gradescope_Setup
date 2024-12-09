9bf4002e98689ea1cb1f9e9047d43536bc44afda
agmui <anthonynmui@gmail.com>
2024-12-09 15:17:26 -0500

    updated run_tests.py for all hw

--- hw/lab01-c_review/run_tests.py
+++ hw/lab01-c_review/run_tests.py
@@ -2,9 +2,7 @@ import unittest
 import os
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
-#TODO: better name
 def editOutput(json_data):
-    #TODO: insert img with html?
     for test_json in json_data["tests"]:
         test_json["output_format"] = "ansi"
         # test_json["output_format"] = "md"
--- hw/lab02-simple_shell/run_tests.py
+++ hw/lab02-simple_shell/run_tests.py
@@ -3,7 +3,6 @@ import os
 from gradescope_utils.autograder_utils.json_test_runner import JSONTestRunner
 
 def editOutput(json_data):
-    #TODO: insert img with html?
     for test_json in json_data["tests"]:
         test_json["output_format"] = "ansi"
         # test_json["output_format"] = "md"
