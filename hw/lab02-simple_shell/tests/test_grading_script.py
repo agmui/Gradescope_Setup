93f28db966705edae3ae0b27d771987ff0d01f0c
agmui <anthonynmui@gmail.com>
2025-01-13 02:41:35 -0500

    updated simple shell to use shared colors config

--- hw/lab02-simple_shell/tests/test_grading_script.py
+++ hw/lab02-simple_shell/tests/test_grading_script.py
@@ -4,18 +4,7 @@ import subprocess
 import unittest
 from gradescope_utils.autograder_utils.decorators import weight, tags, number, partial_credit
 from art import *
-
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+from hw.grading_utils import bcolors
 
 
 os.chdir("src")
