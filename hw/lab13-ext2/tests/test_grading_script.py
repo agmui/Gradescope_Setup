849b7b192c060ace26d7ebd9ccf6da8090cebe88
agmui <anthonynmui@gmail.com>
2025-02-23 04:16:33 -0500

    fixed typo

--- hw/lab13-ext2/tests/test_grading_script.py
+++ hw/lab13-ext2/tests/test_grading_script.py
@@ -19,7 +19,7 @@ class TestIntegration(unittest.TestCase):
     @number("1")
     def test_testcode(self):
         """autograder testcode tests"""
-        process = subprocess.Popen('valgrind  --log-fd=1 --leak-check=yes ./testcode 451_filesystem.ext2'.split(), stdout=subprocess.PIPE,
+        process = subprocess.Popen('valgrind --log-fd=1 --leak-check=yes ./testcode 451_filesystem.ext2'.split(), stdout=subprocess.PIPE,
                                    encoding='UTF-8')
         result, error = process.communicate()
         print(result)
