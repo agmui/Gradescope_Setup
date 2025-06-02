b5b3f10577d64e396bbea76964515722a243ff10
agmui <anthonynmui@gmail.com>
2025-06-02 12:32:26 -0400

    added custom tests file

---
+++ hw/lab08-threads_mutex_basic/src/red_blue_purple_tests.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "red_blue_purple.c"
+
+int red_blue_purple_tests(){
+    printf("hello from inside red_blue_purple_tests.c\n");
+    printf("running custom tests for red_blue_purple.c\n");
+    return 0;
+}
